d0c88dd625586cea20d6a1f7f1bfa2a77c3541fa
Ben Wu <wubin28@gmail.com>
2014-05-02 21:58:59 +0800

    Eliminated the duplicated value 5 from the production code.

--- money-example/src/main/java/tdd/by/example/Dollar.java
+++ money-example/src/main/java/tdd/by/example/Dollar.java
@@ -7,11 +7,11 @@ public class Dollar {
     public int amount;
 
     public Dollar(int amount) {
-
+        this.amount = amount;
     }
 
     public void times(int multiplier) {
-        amount = 5 * 2;
+        amount = amount * 2;
 
     }
 }
