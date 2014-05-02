256d4abe9cafbb42a34f4fe4e863d5cdee2d5f7d
Ben Wu <wubin28@gmail.com>
2014-05-02 18:48:09 +0800

    Generated a blank project using Maven: mvn archetype:generate -DgroupId=tdd.by.example -DartifactId=money-example -DarchetypeArtifactId=maven-archetype-quickstart -Dversion=1.0

---
+++ money-example/src/main/java/tdd/by/example/App.java
@@ -0,0 +1,13 @@
+package tdd.by.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
