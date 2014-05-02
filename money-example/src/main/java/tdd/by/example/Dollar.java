49ec8a32edbdd393f68fadc03e818dccddaf961e
Ben Wu <wubin28@gmail.com>
2014-05-02 21:49:06 +0800

    Moved the setting of field Dollar.amount from object initialization to the Dollar.times() method.

--- money-example/src/main/java/tdd/by/example/Dollar.java
+++ money-example/src/main/java/tdd/by/example/Dollar.java
@@ -4,13 +4,14 @@ package tdd.by.example;
  * Created by benwu on 14-5-2.
  */
 public class Dollar {
-    public int amount = 5 * 2;
+    public int amount;
 
     public Dollar(int amount) {
 
     }
 
     public void times(int multiplier) {
+        amount = 5 * 2;
 
     }
 }
