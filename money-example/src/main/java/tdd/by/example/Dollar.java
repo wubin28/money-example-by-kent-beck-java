5094009050a76b5dd331be48d087723c0aba3d5d
Ben Wu <wubin28@gmail.com>
2014-05-03 22:26:41 +0800

    Implemented the abstract method Money.currency() in the 2 subclasses.

--- money-example/src/main/java/tdd/by/example/Dollar.java
+++ money-example/src/main/java/tdd/by/example/Dollar.java
@@ -15,4 +15,9 @@ public class Dollar extends Money {
         return new Dollar(amount * multiplier);
     }
 
+    @Override
+    public String currency() {
+        return "USD";
+    }
+
 }
--- money-example/src/main/java/tdd/by/example/Franc.java
+++ money-example/src/main/java/tdd/by/example/Franc.java
@@ -12,4 +12,9 @@ public class Franc extends Money {
         return new Franc(amount * multiplier);
     }
 
+    @Override
+    public String currency() {
+        return "CHF";
+    }
+
 }
