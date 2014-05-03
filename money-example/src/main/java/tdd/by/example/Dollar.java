fb61fe4a93f07cf1a08541c6ea2bd99bbea5d86e
Ben Wu <wubin28@gmail.com>
2014-05-03 15:01:12 +0800

    Pulled field Dollar.amount up to the class Money.

--- money-example/src/main/java/tdd/by/example/Dollar.java
+++ money-example/src/main/java/tdd/by/example/Dollar.java
@@ -4,8 +4,6 @@ package tdd.by.example;
  * Created by benwu on 14-5-2.
  */
 public class Dollar extends Money {
-    private int amount;
-
     public Dollar(int amount) {
         this.amount = amount;
     }
--- money-example/src/main/java/tdd/by/example/Money.java
+++ money-example/src/main/java/tdd/by/example/Money.java
@@ -4,4 +4,5 @@ package tdd.by.example;
  * Created by benwu on 14-5-3.
  */
 public class Money {
+    protected int amount;
 }
