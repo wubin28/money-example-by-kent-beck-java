b4792f8516d51b54c78f170199f42d91d3ba3ccf
Ben Wu <wubin28@gmail.com>
2014-05-03 19:42:46 +0800

    Added TODO-sin: Money rounding?

--- money-example/src/main/java/tdd/by/example/Money.java
+++ money-example/src/main/java/tdd/by/example/Money.java
@@ -4,6 +4,7 @@ package tdd.by.example;
  * Created by benwu on 14-5-3.
  */
 public class Money {
+    // TODO-sin: Money rounding?
     protected int amount;
 
     public boolean equals(Object object) {
