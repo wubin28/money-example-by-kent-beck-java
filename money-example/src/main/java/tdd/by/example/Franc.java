7ce36b899de11e5c10957f484c700d9824febfdb
Ben Wu <wubin28@gmail.com>
2014-05-03 15:30:52 +0800

    Deleted Franc's field amount in favour of the one in Money.

--- money-example/src/main/java/tdd/by/example/Franc.java
+++ money-example/src/main/java/tdd/by/example/Franc.java
@@ -4,8 +4,6 @@ package tdd.by.example;
  * Created by benwu on 14-5-3.
  */
 public class Franc extends Money {
-    private int amount;
-
     public Franc(int amount) {
         this.amount = amount;
     }
