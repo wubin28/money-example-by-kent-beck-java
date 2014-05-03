7a387cbb3abc1b6b8efdb68eb0291d8f257f7269
Ben Wu <wubin28@gmail.com>
2014-05-03 13:55:29 +0800

    Made the field Dollar.amount private.

--- money-example/src/main/java/tdd/by/example/Dollar.java
+++ money-example/src/main/java/tdd/by/example/Dollar.java
@@ -4,7 +4,7 @@ package tdd.by.example;
  * Created by benwu on 14-5-2.
  */
 public class Dollar {
-    public int amount;
+    private int amount;
 
     public Dollar(int amount) {
         this.amount = amount;
