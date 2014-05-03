1f852f5b371f4b70452e72d6647253833fc8ee3c
Ben Wu <wubin28@gmail.com>
2014-05-03 20:47:08 +0800

    Working on TODO-working-on: Dollar/Franc duplication.

--- money-example/src/main/java/tdd/by/example/Dollar.java
+++ money-example/src/main/java/tdd/by/example/Dollar.java
@@ -4,7 +4,7 @@ package tdd.by.example;
  * Created by benwu on 14-5-2.
  */
 public class Dollar extends Money {
-    // TODO-sin: Dollar/Franc duplication
+    // TODO-working-on: Dollar/Franc duplication
 
     public Dollar(int amount) {
         this.amount = amount;
