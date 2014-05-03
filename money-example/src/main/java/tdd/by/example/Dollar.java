b582352e15c5c4923eb3fa83ab2920dc01d37005
Ben Wu <wubin28@gmail.com>
2014-05-03 14:57:06 +0800

    Made class Dollar extend Money.

--- money-example/src/main/java/tdd/by/example/Dollar.java
+++ money-example/src/main/java/tdd/by/example/Dollar.java
@@ -3,7 +3,7 @@ package tdd.by.example;
 /**
  * Created by benwu on 14-5-2.
  */
-public class Dollar {
+public class Dollar extends Money {
     private int amount;
 
     public Dollar(int amount) {
