479306e853309f1b5799cbb7a1a0d1a5400f7ab4
Ben Wu <wubin28@gmail.com>
2014-05-04 19:29:32 +0800

    Working on TODO-sin-working-on: Common times.

--- money-example/src/main/java/tdd/by/example/Dollar.java
+++ money-example/src/main/java/tdd/by/example/Dollar.java
@@ -11,7 +11,7 @@ public class Dollar extends Money {
         super(amount, currency);
     }
 
-    // TODO-sin: Common times
+    // TODO-sin-working-on: Common times
     public Money times(int multiplier) {
         return Money.dollar(amount * multiplier);
     }
