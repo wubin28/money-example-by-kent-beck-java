af560624f1bdd84ef256cccd16eb70f198b865b6
Ben Wu <wubin28@gmail.com>
2014-05-04 13:28:14 +0800

    Extracted the constant return value of method Dollar.currency() to a field of the class.

--- money-example/src/main/java/tdd/by/example/Dollar.java
+++ money-example/src/main/java/tdd/by/example/Dollar.java
@@ -4,6 +4,7 @@ package tdd.by.example;
  * Created by benwu on 14-5-2.
  */
 public class Dollar extends Money {
+    private String currency;
     // TODO-sin: Dollar/Franc duplication
 
     public Dollar(int amount) {
@@ -17,7 +18,8 @@ public class Dollar extends Money {
 
     @Override
     public String currency() {
-        return "USD";
+        currency = "USD";
+        return currency;
     }
 
 }
