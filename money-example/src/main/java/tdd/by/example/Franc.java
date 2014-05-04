8720749e3397dad2cb6c6fde995ed669f3e38ebc
Ben Wu <wubin28@gmail.com>
2014-05-04 18:47:57 +0800

    Assigned the formal parameter to the instance field in the constructor of the class Franc.

--- money-example/src/main/java/tdd/by/example/Franc.java
+++ money-example/src/main/java/tdd/by/example/Franc.java
@@ -7,7 +7,7 @@ public class Franc extends Money {
 
     public Franc(int amount, String currency) {
         this.amount = amount;
-        this.currency = "CHF";
+        this.currency = currency;
     }
 
     public Money times(int multiplier) {
