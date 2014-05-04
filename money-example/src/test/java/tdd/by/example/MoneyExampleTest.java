aa8b205e077212b2b4dba71c0e2caab271646809
Ben Wu <wubin28@gmail.com>
2014-05-04 22:48:10 +0800

    Added testDifferentClassEquality().

--- money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
+++ money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
@@ -42,4 +42,9 @@ public class MoneyExampleTest {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
     }
+
+    @Test
+    public void testDifferentClassEquality() {
+        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
+    }
 }
