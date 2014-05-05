6e98c55610b543e76eec5141ced82c0a346fc5ae
Ben Wu <wubin28@gmail.com>
2014-05-05 08:49:00 +0800

    Replaced the references to the subclasses with the references to the superclass without changing the meaning of the code to get started to delete the 2 subclasses.

--- money-example/src/main/java/tdd/by/example/Money.java
+++ money-example/src/main/java/tdd/by/example/Money.java
@@ -24,7 +24,7 @@ public class Money {
     }
 
     public static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     public Money times(int multiplier) {
@@ -32,7 +32,7 @@ public class Money {
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     public String currency() {
