3c38f7040b7d824f0d9bf032e3805d4ef8162452
Ben Wu <wubin28@gmail.com>
2014-05-06 20:46:48 +0800

    Set the fields in Sum's constructor.

--- money-example/src/main/java/tdd/by/example/Sum.java
+++ money-example/src/main/java/tdd/by/example/Sum.java
@@ -8,6 +8,7 @@ public class Sum implements Expression{
     public Money addend;
 
     public Sum(Money augend, Money addend) {
-
+        this.augend = augend;
+        this.addend = addend;
     }
 }
