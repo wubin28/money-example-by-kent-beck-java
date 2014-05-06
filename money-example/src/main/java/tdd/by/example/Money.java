2202039c748bdec983ba3ca5dd2bf35430215b2b
Ben Wu <wubin28@gmail.com>
2014-05-06 09:54:56 +0800

    Money.plus() needs to return an Expression.

--- money-example/src/main/java/tdd/by/example/Money.java
+++ money-example/src/main/java/tdd/by/example/Money.java
@@ -39,7 +39,7 @@ public class Money {
         return currency;
     }
 
-    public Money plus(Money addend) {
+    public Expression plus(Money addend) {
         return new Money(amount + addend.amount, currency());
     }
 }
