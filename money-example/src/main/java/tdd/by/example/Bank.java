958dffc9fc1c53d955331dcaac4af6eb8a5c080a
Ben Wu <wubin28@gmail.com>
2014-05-06 21:15:53 +0800

    Updated method Bank.reduce() to calculate the sum.

--- money-example/src/main/java/tdd/by/example/Bank.java
+++ money-example/src/main/java/tdd/by/example/Bank.java
@@ -5,6 +5,8 @@ package tdd.by.example;
  */
 public class Bank {
     public Money reduce(Expression source, String to) {
-        return Money.dollar(10);
+        Sum sum = (Sum)source;
+        int amount = sum.augend.amount + sum.addend.amount;
+        return new Money(amount, to);
     }
 }
