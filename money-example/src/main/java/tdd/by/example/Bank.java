b31e5a5beca4465cbe586992900717e03877aece
Ben Wu <wubin28@gmail.com>
2014-05-07 09:01:09 +0800

    Eliminated all those ugly casts and class checks in Bank.reduce().

--- money-example/src/main/java/tdd/by/example/Bank.java
+++ money-example/src/main/java/tdd/by/example/Bank.java
@@ -5,8 +5,6 @@ package tdd.by.example;
  */
 public class Bank {
     public Money reduce(Expression source, String to) {
-        if (source instanceof Money) return (Money)source.reduce(to);
-        Sum sum = (Sum)source;
-        return sum.reduce(to);
+        return source.reduce(to);
     }
 }
