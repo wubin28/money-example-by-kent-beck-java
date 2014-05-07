87eb8ec3362ca057782e5cd68cc66a5c477d4148
Ben Wu <wubin28@gmail.com>
2014-05-07 19:55:01 +0800

    Updated method Money.reduce() to use rate in a ugly way.

--- money-example/src/main/java/tdd/by/example/Money.java
+++ money-example/src/main/java/tdd/by/example/Money.java
@@ -45,6 +45,9 @@ public class Money implements Expression {
     }
 
     public Money reduce(String to) {
-        return this;
+        int rate = (currency.equals("CHF") && to.equals("USD"))
+                ? 2
+                : 1;
+        return new Money(amount / rate, to);
     }
 }
