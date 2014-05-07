90a42fa38475393852a31412eac4f457932141d9
Ben Wu <wubin28@gmail.com>
2014-05-07 08:21:18 +0800

    Moved the calculation of amount from Bank.reduce() to Sum.reduce() to fix the 2 code smells: 1)the cast (Sum); 2)the public fields and two levels of references at that.

--- money-example/src/main/java/tdd/by/example/Bank.java
+++ money-example/src/main/java/tdd/by/example/Bank.java
@@ -6,7 +6,6 @@ package tdd.by.example;
 public class Bank {
     public Money reduce(Expression source, String to) {
         Sum sum = (Sum)source;
-        int amount = sum.augend.amount + sum.addend.amount;
-        return new Money(amount, to);
+        return sum.reduce(to);
     }
 }
