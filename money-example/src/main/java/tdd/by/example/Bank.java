e273723405a7cb68b1ba2a172ffa7896c5959b8b
Ben Wu <wubin28@gmail.com>
2014-05-07 19:59:28 +0800

    Stubbed method Bank.addRate().

--- money-example/src/main/java/tdd/by/example/Bank.java
+++ money-example/src/main/java/tdd/by/example/Bank.java
@@ -7,4 +7,8 @@ public class Bank {
     public Money reduce(Expression source, String to) {
         return source.reduce(to);
     }
+
+    public void addRate(String from, String to, int rate) {
+
+    }
 }
