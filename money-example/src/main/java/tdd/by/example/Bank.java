05656ced73272cd8f83437a1967ba1e9383e2a71
Ben Wu <wubin28@gmail.com>
2014-05-07 20:25:00 +0800

    Moved the calculation of rate to the method Bank.rate().

--- money-example/src/main/java/tdd/by/example/Bank.java
+++ money-example/src/main/java/tdd/by/example/Bank.java
@@ -11,4 +11,10 @@ public class Bank {
     public void addRate(String from, String to, int rate) {
 
     }
+
+    public int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD"))
+                ? 2
+                : 1;
+    }
 }
