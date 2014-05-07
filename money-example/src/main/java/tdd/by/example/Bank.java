6ac8ca51240e88145b4e33ffd3ab0b4673d299bb
Ben Wu <wubin28@gmail.com>
2014-05-07 08:39:55 +0800

    Updated Bank.reduce() to return Money directly if the input is a Money. Ugly.

--- money-example/src/main/java/tdd/by/example/Bank.java
+++ money-example/src/main/java/tdd/by/example/Bank.java
@@ -5,6 +5,7 @@ package tdd.by.example;
  */
 public class Bank {
     public Money reduce(Expression source, String to) {
+        if (source instanceof Money) return (Money)source;
         Sum sum = (Sum)source;
         return sum.reduce(to);
     }
