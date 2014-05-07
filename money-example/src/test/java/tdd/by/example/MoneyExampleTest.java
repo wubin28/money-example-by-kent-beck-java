e65745411e1a6940a9f451a3d13b1dab826a05d3
Ben Wu <wubin28@gmail.com>
2014-05-07 08:34:21 +0800

    Added testReduceMoney() for TODO-story: Bank.reduce(Money).

--- money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
+++ money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
@@ -53,12 +53,19 @@ public class MoneyExampleTest {
         assertEquals(five, sum.addend);
     }
 
-    // TODO-story: Bank.reduce(Money)
     @Test
     public void testReduceSum() {
         Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
         Bank bank = new Bank();
         Money result = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(7), result);
+    }
+
+    // TODO-story: Bank.reduce(Money)
+    @Test
+    public void testReduceMoney() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
     }
 }
