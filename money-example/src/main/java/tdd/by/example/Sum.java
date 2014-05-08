c1e67ba5b0c2afd076e43c47895a56d3fd73d339
Ben Wu <wubin28@gmail.com>
2014-05-08 08:23:25 +0800

    Changed the arguments to the Sum constructor to be Expressions.

--- money-example/src/main/java/tdd/by/example/Sum.java
+++ money-example/src/main/java/tdd/by/example/Sum.java
@@ -7,7 +7,7 @@ public class Sum implements Expression{
     public Expression augend;
     public Expression addend;
 
-    public Sum(Money augend, Money addend) {
+    public Sum(Expression augend, Expression addend) {
         this.augend = augend;
         this.addend = addend;
     }
