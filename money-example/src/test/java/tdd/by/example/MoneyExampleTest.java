500b0963096f3a8d263f364a9333c29c9e52ac20
Ben Wu <wubin28@gmail.com>
2014-05-10 09:33:14 +0800

    Added testPlusSameCurrencyReturnsMoney().

--- money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
+++ money-example/src/test/java/tdd/by/example/MoneyExampleTest.java
@@ -109,4 +109,10 @@ public class MoneyExampleTest {
         Money result = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(20), result);
     }
+
+    @Test
+    public void testPlusSameCurrencyReturnsMoney() {
+        Expression sum = Money.dollar(1).plus(Money.dollar(1));
+        assertTrue(sum instanceof Money);
+    }
 }
