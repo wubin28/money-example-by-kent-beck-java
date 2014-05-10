851c82133958dff3c687f40d8950ccea68dae9f4
Ben Wu <wubin28@gmail.com>
2014-05-10 08:34:44 +0800

    Implemented method Sum.plus() which was the same as the method Money.plus().

--- money-example/src/main/java/tdd/by/example/Sum.java
+++ money-example/src/main/java/tdd/by/example/Sum.java
@@ -20,6 +20,6 @@ public class Sum implements Expression{
     // TODO-sin-working-on: Sum.plus
     @Override
     public Expression plus(Expression addend) {
-        return null;
+        return new Sum(this, addend);
     }
 }
